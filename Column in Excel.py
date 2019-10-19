09332b9f38c262a80e4d2e413698304e059934fd
Quicksand <u1304002@student.cuet.ac.bd>
2019-10-19 15:45:03 +0600

    excel created and written into

--- Column in Excel.py
+++ Column in Excel.py
@@ -1,28 +1,8 @@
 import os
 import argparse
 import re
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument("square",type=int,help="display square of a given number") #positional argument(must have to provide from command line while running)
-# parser.add_argument("--verbose", help="verbosity turned on",action = "store_true")
-# args = parser.parse_args()
-# if args.verbose:
-#     print ("the square of {} eqauls {}".format(args.square,args.square**2))
-# else:
-#     print(args.square**2)
-# table_info ={
-#     'table_name':'',
-#     'columns':'',
-#     'primary_key'='',
-#     'unique'='',
-#     'auto_increment':'',
-#     'foreign_key':'',
-#     'foreign_key_table'='',
-#     'foreign-_key_column'='',
-#     'not_null':'',
-#     'default':'',
-#     'check':''
-# }
+import pprint
+import pandas as pd
 
 def isUnique(line):
     temp = re.search(r'\w*(unique=)(True|False)\w*',line)
@@ -86,26 +66,57 @@ def datatype(line):
         return temp[1]
 
 
-def build_dict(in_dir):
+def build_dict(in_dir,out_dir):
+
+    column_list =[]
+    unique_list =[]
+    primary_key_list = []
+    autoIncrement_list=[]
+    nullable_list =[]
+    datatype_list=[]
+    table_name=''
+
     with open(in_dir,'r') as f:
         for line in f:
             line=line.strip().split()
             per_line=''
+            column_name=''
             for line in line:
                 per_line +=line
-            # print(per_line)
-            table_name = isTable(per_line)
+            if table_name == '':
+                table_name = isTable(per_line)
             column_name=isColumn(per_line)
-            data_type = datatype(per_line)
-            is_unique = isUnique(per_line)
+            if column_name is not '':
+                column_list.append(column_name)
+                datatype_list.append(datatype(per_line))
+                unique_list.append(isUnique(per_line)) 
+                nullable_list.append(isNullable(line)) 
+                primary_key_list.append(isPrimaryKey(line)) 
+                autoIncrement_list.append(autoIncrement(line)) 
+        f.close()
+
+    excel_dict = {
+        'column':column_list,
+        'datatype':datatype_list,
+        'unique':unique_list,
+        'nullable':nullable_list,
+        'primary_key':primary_key_list,
+        'autoincrement':autoIncrement_list
+    }
+
+    # pprint.pprint(excel_dict)
+    final = pd.DataFrame(excel_dict)
+    writer = pd.ExcelWriter(os.path.join(out_dir,'tables.xlsx'),engine='xlsxwriter')
+    final.to_excel(writer, sheet_name=table_name)
+    writer.save()
 
         
 def change(args):
     in_dir=os.path.join(args.base_dir,args.change)
     out_dir=os.path.join(os.getcwd(),args.output)
     os.makedirs(out_dir,exist_ok=True)
-    build_dict(in_dir)
-    print(out_dir)
+    build_dict(in_dir,out_dir)
+    # print(out_dir)
     
 
 def main():
